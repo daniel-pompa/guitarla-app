56e5da28d94b953bc844b3ce4a3f079637239fe9
Daniel <daniel.40.pompa@gmail.com>
2024-08-18 14:34:05 +0200

    feat: add guitars data
    Add detailed guitar information including names, images, descriptions, and prices for multiple models.

---
+++ src/data/data.js
@@ -0,0 +1,98 @@
+export const guitarData = [
+  {
+    id: 1,
+    name: 'Lukather',
+    image: 'guitar-01',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus imperdiet nulla nec quam sollicitudin, non vulputate ligula facilisis.',
+    price: 289,
+  },
+  {
+    id: 2,
+    name: 'SRV',
+    image: 'guitar-02',
+    description:
+      'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    price: 359,
+  },
+  {
+    id: 3,
+    name: 'Borland',
+    image: 'guitar-03',
+    description:
+      'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.',
+    price: 339,
+  },
+  {
+    id: 4,
+    name: 'VAI',
+    image: 'guitar-04',
+    description:
+      'Curabitur pretium tincidunt lacus. Nulla gravida orci a libero suscipit, quis commodo velit sodales. Morbi congue magna non justo elementum fermentum.',
+    price: 309,
+  },
+  {
+    id: 5,
+    name: 'Thompson',
+    image: 'guitar-05',
+    description:
+      'Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Suspendisse potenti. Integer ac tristique arcu, a laoreet lorem.',
+    price: 389,
+  },
+  {
+    id: 6,
+    name: 'White',
+    image: 'guitar-06',
+    description:
+      'Maecenas vel orci eu metus bibendum varius. Nulla facilisi. Aliquam at diam ac erat vehicula interdum.',
+    price: 339,
+  },
+  {
+    id: 7,
+    name: 'Cobain',
+    image: 'guitar-07',
+    description:
+      'Phasellus euismod augue eu ligula pretium scelerisque. Integer accumsan eros et est consectetur, at elementum nisl tristique.',
+    price: 359,
+  },
+  {
+    id: 8,
+    name: 'Dale',
+    image: 'guitar-08',
+    description:
+      'Fusce sit amet venenatis nunc. Donec a orci sit amet nunc vestibulum eleifend. Nullam efficitur lacinia velit, ut pretium erat.',
+    price: 379,
+  },
+  {
+    id: 9,
+    name: 'Krieger',
+    image: 'guitar-09',
+    description:
+      'Donec quis velit et quam tincidunt volutpat. Cras ut nisi tristique, bibendum eros a, consectetur justo.',
+    price: 299,
+  },
+  {
+    id: 10,
+    name: 'Campbell',
+    image: 'guitar-10',
+    description:
+      'Integer fermentum nisl ac justo egestas, non faucibus nunc tincidunt. Curabitur condimentum nisi id bibendum dictum.',
+    price: 359,
+  },
+  {
+    id: 11,
+    name: 'Reed',
+    image: 'guitar-11',
+    description:
+      'Mauris vehicula lectus non urna volutpat, ac efficitur lectus consectetur. Nulla in mauris at eros tempor interdum.',
+    price: 389,
+  },
+  {
+    id: 12,
+    name: 'Hazel',
+    image: 'guitar-12',
+    description:
+      'Praesent lacinia quam ut massa sollicitudin, sed tempus risus tincidunt. Sed venenatis justo at lectus fermentum, ut cursus ligula laoreet.',
+    price: 379,
+  },
+];
